bc284cb76857f5f84279750edf9118579466fb2d
Ashna-Git <120472446+Ashna-Git@users.noreply.github.com>
2022-12-14 12:17:50 +0530

    Update streamlit_app.py
    Adding list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,14 @@
 import streamlit
+import pandas
+
 streamlit.title('My new tutorial')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🎂Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥙Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍽Hard-Boiled Free-Range Egg')
+streamlit.text('Build Your own Fruit Smoothie')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
