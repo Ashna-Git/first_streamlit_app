0f49382caaf082093f82f396adbb54940986d6db
Ashna-Git <120472446+Ashna-Git@users.noreply.github.com>
2022-12-21 15:14:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,5 @@ streamlit.dataframe(my_data_rows)
 fruit_choice = streamlit.text_input('What fruit would you like add?','Jackfruit')
 streamlit.write('Thanks for Adding ', fruit_choice)
 
+my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
+
